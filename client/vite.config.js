893716aea682656daeed9c9dcfa2bbfacf978f37
Aditya <adityapradeepjadhav52@gmail.com>
2025-06-17 23:22:57 +0530

    # remove eslint

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import eslint from 'vite-plugin-eslint'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-      tailwindcss(),
-      react()],
+    tailwindcss(),
+    react(),
+    eslint({
+      failOnWarning: false,
+      failOnError: false, // 👈 this line makes ESLint errors non-blocking
+    }),
+  ],
 })
-
